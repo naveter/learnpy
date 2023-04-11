8f1ebd4a297014e5827d5a7f1b337ff010beff27
naveter <naveter@gmail.com>
2023-04-11 16:33:12 +0300

    Added some format example

--- src/tutorial/iostaff.py
+++ src/tutorial/iostaff.py
@@ -39,6 +39,17 @@ print('-3.14'.zfill(7))     # fill with zero lead
 print('The value of pi is approximately %5.3f.' % math.pi)
 
 
+# format words into equals cols
+def split_names_into_rows(name_list, modulus=3):
+    for index, name in enumerate(name_list, start=1):   # first index not 0 but 1
+        print(f"{name:-^15} ", end="")
+        if index % modulus == 0:
+            print()
+    print()
+
+
+names = ["Picard", "Riker", "Troi", "Crusher", "Worf", "Data", "La Forge"]
+split_names_into_rows(names)
 
 
 
